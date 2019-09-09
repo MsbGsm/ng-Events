b7f5d6812ec8b198bff69b516f304fd139182236
MsbGsm <msbgsm@gmail.com>
2019-09-10 00:20:37 +0100

    Filtering Data

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { ISession } from '../shared';
 
 @Component({
@@ -6,6 +6,24 @@ import { ISession } from '../shared';
     templateUrl: './session-list.component.html'
 })
 
-export class SessionListComponent {
-    @Input() sessions:ISession[]
+export class SessionListComponent implements OnChanges {
+    @Input() sessions:ISession[];
+    @Input() filterBy: string;
+    visibleSessions: ISession[] = [];
+
+    ngOnChanges() {
+        if (this.sessions) {
+            this.filterSessions(this.filterBy);
+        }
+    }
+
+    filterSessions(filter: string) {
+        if (filter === 'all') {
+            this.visibleSessions = this.sessions.slice(0);
+        } else {
+            this.visibleSessions = this.sessions.filter(session => {
+                return session.level.toLocaleLowerCase() === filter;
+            });
+        }
+    }
 }
