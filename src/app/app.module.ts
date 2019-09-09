b3614887a59556d2f1135443c8a2d5b98559d63f
MsbGsm <msbgsm@gmail.com>
2019-09-09 20:55:31 +0100

    Passing Data Into A Child Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,66 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {
+  EventListComponent,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventService,
+  EventRouteActivator,
+  EventListResolver, 
+  CreateSessionComponent} from './events/index';
+
+import { EventAppComponent } from './event-app.component';
+import { Error404Component } from './errors/404.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { ToastrService } from './common/toastr.service';
+import { appRoutes } from './routes';
+import { AuthService } from './user/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SessionListComponent } from './events/event-details/session-list.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    EventAppComponent,
+    EventListComponent,
+    EventThumbnailComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    CreateSessionComponent,
+    SessionListComponent,
+    Error404Component,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    EventListResolver,
+    AuthService,
+    {
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkDirtyState
+    }
+  ],
+  bootstrap: [EventAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty) {
+    return window.confirm('You have not saved this event, do you really want to cancel?')
+  }
+  return true;
+}
---
+++ src/app/event-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'event-app',
+  template: `
+    <nav-bar></nav-bar>
+    <router-outlet></router-outlet>
+  `
+})
+export class EventAppComponent {
+  title = 'app';
+}
