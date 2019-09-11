353d3af37ae6934b1a4da37b6f80baf352898f0d
MsbGsm <msbgsm@gmail.com>
2019-09-11 23:29:32 +0100

    Creating Settings On Components

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -27,6 +27,7 @@ import { JQ_TOKEN } from './index';
 export class SimpleModalComponent {
     @Input() title: string;
     @Input() elementId: string;
+    @Input() closeOnBodyClick: string;
     @ViewChild('modalcontainer') containerEl: ElementRef;
 
     constructor(@Inject(JQ_TOKEN) private $: any) {
@@ -34,6 +35,8 @@ export class SimpleModalComponent {
     }
 
     closeModal() {
-        this.$(this.containerEl.nativeElement).modal('hide');
+        if (this.closeOnBodyClick.toLocaleLowerCase() === "true") {
+            this.$(this.containerEl.nativeElement).modal('hide');
+        }
     }
 }
