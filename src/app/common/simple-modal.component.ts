2b1d2cde5b0f78df666a2a341e11ed83738fc888
MsbGsm <msbgsm@gmail.com>
2019-09-11 23:08:35 +0100

    Using The @ViewChild Decorator

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, Input, Inject, ElementRef, ViewChild } from '@angular/core';
+import { JQ_TOKEN } from './index';
 @Component({
     selector: 'simple-modal',
     template: `
-    <div id="{{elementId}}" class="modal fade" tabindex="-1">
+    <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -12,7 +12,7 @@ import { Component, Input } from '@angular/core';
                     </button>
                     <h4 class="modal-title">{{title}}</h4> 
                 </div>
-                <div class="modal-body">
+                <div class="modal-body" (click)="closeModal()">
                     <ng-content></ng-content>
                 </div>
             </div>
@@ -27,4 +27,13 @@ import { Component, Input } from '@angular/core';
 export class SimpleModalComponent {
     @Input() title: string;
     @Input() elementId: string;
+    @ViewChild('modalcontainer') containerEl: ElementRef;
+
+    constructor(@Inject(JQ_TOKEN) private $: any) {
+
+    }
+
+    closeModal() {
+        this.$(this.containerEl.nativeElement).modal('hide');
+    }
 }
